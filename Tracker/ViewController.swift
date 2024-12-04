e78682a4ad183670d9078a096deb41df88b2056a
Rodion Kim <skyrood2@yandex.ru>
2024-12-04 20:29:55 +0900

    Initial Commit

---
+++ Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Rodion Kim on 2024/12/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
